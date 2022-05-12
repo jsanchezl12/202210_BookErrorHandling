a9b6e4764cbb2dd30a6eb7e02a4fa7310f9c8991
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-05-12 11:12:38 -0400

    Changes to take in count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; //http interceptors
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ToastrModule } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr'; // Toast
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { EditorialModule } from './editorial/editorial.module';
-import { HttpErrorInterceptorService } from './interceptors/http-error-interceptor.service';
+import { HttpErrorInterceptorService } from './interceptors/http-error-interceptor.service'; //http Error interceptor service
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -23,14 +23,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     EditorialModule,
     AuthorModule,
     HttpClientModule,
+    //toast module
     ToastrModule.forRoot({
       timeOut: 10000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
       }
     ),
+    //browser animations module
     BrowserAnimationsModule
   ],
+  //providers
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- src/app/interceptors/http-error-interceptor.service.ts
+++ src/app/interceptors/http-error-interceptor.service.ts
@@ -1,21 +1,19 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpRequest,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { ToastrService } from 'ngx-toastr';
+import {HttpEvent,HttpHandler,HttpRequest,HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs'; //throw error
+import { catchError } from 'rxjs/operators'; // catch error
+import { ToastrService } from 'ngx-toastr'; //ToastrService
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HttpErrorInterceptorService extends HttpErrorResponse {
+  //toast service in constructor
   constructor(private toastrService: ToastrService) { super(toastrService) }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
         catchError((httpErrorResponse: HttpErrorResponse) => {
+          console.log(httpErrorResponse.error.message);
+          //nuevo codigo
           let errorMesagge = '';
           let errorType = '';
 
@@ -29,6 +27,7 @@ export class HttpErrorInterceptorService extends HttpErrorResponse {
             } else {
               errorMesagge = `${httpErrorResponse.status}: ${httpErrorResponse.error.error}`;
             }
+            // Mensaje de error
             this.toastrService.error(errorMesagge, errorType, { closeButton: true });
           }
           return throwError(()=> new Error(errorMesagge));
